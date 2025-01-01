6eaa6488f9693a28a1edf5fd9f3111800ed0b06f
Jj <keistaslogairkosulys@gmail.com>
2025-01-01 16:53:55 +0100

    init

---
+++ lein-carrinho-de-cavalo/project.clj
@@ -0,0 +1,12 @@
+(defproject org.clojars.jj/lein-carrinho-de-cavalo "1.0.3-SNAPSHOT"
+  :description "FIXME: write description possible Carroça\n
+  Carro de boi
+  Carro cavalo
+  charrete de cavalos\n"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [info.sunng/ring-jetty9-adapter "0.36.1"]
+                 [ring/ring-core "1.13.0"]]
+  :repl-options {:init-ns cavalo-sidecar.core})
