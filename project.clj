3c12102e20c89ec2629a7e0660bee3b3da16d966
ruroru <111705692+ruroru@users.noreply.github.com>
2026-02-07 22:30:50 +0000

    Stepping project version for the next development cycle

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.jj/lein-cavalo-parent "1.0.7"
+(defproject org.clojars.jj/lein-cavalo-parent "1.0.8-SNAPSHOT"
   :description "Leiningen plugin to reload page on file change"
   :url "https://github.com/ruroru/lein-cavalo"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :dependencies [[org.clojars.jj/lein-cavalo "1.0.7"]
-                 [org.clojars.jj/lein-carroca "1.0.7"]
+  :dependencies [[org.clojars.jj/lein-cavalo "1.0.8-SNAPSHOT"]
+                 [org.clojars.jj/lein-carroca "1.0.8-SNAPSHOT"]
                  [leiningen-core "2.12.0"]]
 
   :plugins [[lein-sub "0.3.0"]
