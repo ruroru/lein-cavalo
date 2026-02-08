2f1916809de4ef9123216a2953c7fdbb46f4385a
jj <keistaslogairkosulys@gmail.com>
2026-02-08 17:17:54 +0100

    update scm

--- lein-carroca/project.clj
+++ lein-carroca/project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/ruroru/lein-cavalo"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :scm {:dir ".."}
+  :scm {:name "git"
+        :url  "https://github.com/ruroru/lein-cavalo"
+        :dir  ".."}
 
   :dependencies [[org.clojure/clojure "1.12.4"]
                  [org.clojure/tools.logging "1.3.1"]
--- lein-cavalo/project.clj
+++ lein-cavalo/project.clj
@@ -2,7 +2,10 @@
   :description "Cavalo is a leiningen plugin to reload http servers on file change."
   :url "https://github.com/ruroru/lein-cavalo"
 
-  :scm {:dir ".."}
+  :scm {:name "git"
+        :url  "https://github.com/ruroru/lein-cavalo"
+        :dir  ".."}
+
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.4"]
